cb2444ac2b7c617e2fd300b8ef71703f8c8fb8bf
Hasan <123514871+imhasandl@users.noreply.github.com>
2025-07-23 22:12:52 +0300

    Add README and implement SSH file upload/download functionality

--- go.mod
+++ go.mod
@@ -1,3 +1,13 @@
 module github.com/imhasandl/package-manager
 
 go 1.23.5
+
+require (
+	github.com/pkg/sftp v1.13.9
+	golang.org/x/crypto v0.40.0
+)
+
+require (
+	github.com/kr/fs v0.1.0 // indirect
+	golang.org/x/sys v0.34.0 // indirect
+)
--- main.go
+++ main.go
@@ -7,19 +7,24 @@ import (
 
 	"github.com/imhasandl/package-manager/archive"
 	"github.com/imhasandl/package-manager/config"
+	"github.com/imhasandl/package-manager/ssh"
 )
 
 func main() {
-	fmt.Println("Использование:")
-	fmt.Println("pm create <packet.json>")
-	fmt.Println("pm update <packages.json>")
+	if len(os.Args) < 2 {
+		fmt.Println("Использование:")
+		fmt.Println("  pm create <packet.json>")
+		fmt.Println("  pm update <packages.json>")
+		return
+	}
 
 	command := os.Args[1]
 
 	switch command {
 	case "create":
 		if len(os.Args) < 3 {
-			fmt.Println("pm create <package.json>")
+			fmt.Println("Использование: pm create <packet.json>")
+			return
 		}
 		err := CreatePackage(os.Args[2])
 		if err != nil {
@@ -27,19 +32,23 @@ func main() {
 		}
 	case "update":
 		if len(os.Args) < 3 {
-			fmt.Println("pm update <package.json>")
+			fmt.Println("Использование: pm update <packages.json>")
+			return
 		}
 		err := UpdatePackage(os.Args[2])
 		if err != nil {
 			log.Printf("Не получилось обновить архив: %v", err)
 		}
+	default:
+		fmt.Printf("Неизвестная команда: %s\n", command)
+		fmt.Println("Доступные команды: create, update")
 	}
 }
 
 func CreatePackage(configFile string) error {
 	config, err := config.ReadPackageConfig(configFile)
 	if err != nil {
-		return fmt.Errorf("ошибка чтения конфига:", err)
+		return fmt.Errorf("ошибка чтения конфига: %v", err)
 	}
 
 	packageName := config["name"].(string)
@@ -51,10 +60,66 @@ func CreatePackage(configFile string) error {
 		return fmt.Errorf("ошибка создания архива: %v", err)
 	}
 
+	remotePath := "/packages/" + archiveFile
+	err = ssh.UploadFile(archiveFile, remotePath)
+	if err != nil {
+		return fmt.Errorf("ошибка загрузки: %v", err)
+	}
+
+	fmt.Printf("✓ Пакет %s успешно создан и загружен!\n", packageName)
 	return nil
 }
 
 func UpdatePackage(configFile string) error {
+	config, err := config.ReadPackagesConfig(configFile)
+	if err != nil {
+		return fmt.Errorf("ошибка чтения конфига: %v", err)
+	}
+
+	packages := config["packages"].([]interface{})
+
+	for _, pkg := range packages {
+		pkgMap := pkg.(map[string]interface{})
+		packageName := pkgMap["name"].(string)
+
+		var packageVersion string
+		if ver, exists := pkgMap["ver"]; exists {
+			packageVersion = ver.(string)
+		}
+
+		err := downloadAndInstallPackage(packageName, packageVersion)
+		if err != nil {
+			log.Printf("Ошибка установки пакета %s: %v", packageName, err)
+			continue
+		}
+		fmt.Printf("✓ Пакет %s успешно установлен\n", packageName)
+	}
+
+	fmt.Println("Обновление завершено!")
+	return nil
+}
+
+func downloadAndInstallPackage(packageName, packageVersion string) error {
+	var archiveFile string
+	if packageVersion != "" {
+		archiveFile = fmt.Sprintf("%s-%s.zip", packageName, packageVersion)
+	} else {
+		archiveFile = packageName + "-latest.zip"
+	}
+
+	remotePath := "/packages/" + archiveFile
+	localPath := "./downloads/" + archiveFile
+
+	err := ssh.DownloadFile(remotePath, localPath)
+	if err != nil {
+		return fmt.Errorf("не получилось скачать файл: %v", err)
+	}
+
+	extractPath := "./packages/" + packageName
+	err = archive.ExtractZipArchive(localPath, extractPath)
+	if err != nil {
+		return fmt.Errorf("не удалось распаковать: %v", err)
+	}
 
 	return nil
 }
